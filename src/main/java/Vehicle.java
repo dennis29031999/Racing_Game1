64eb7dfa8cda1a380c13b18c3565b40b9a5dea4d
Denis <denissirghi99@gmail.com>
2021-12-29 15:28:14 +0200

    6

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -12,5 +12,17 @@ public class Vehicle
     boolean damage;
     String color;
 
+    public double accelerate(double speed, double durationInHours){
+        System.out.println(name+ " is accelerating with "+ speed + "kh/h for "+ durationInHours+ "h");
+        double distance= speed * durationInHours;
+        totalTraveledDistance += distance;
+        System.out.println("Total traveled distance for vehicle "+ name +": "+ totalTraveledDistance);
+        double usedFuel= distance * mileage / 100;
+        fuelLevel -= usedFuel;
+        System.out.println("Reamaning fuel for vehicle "+ name +": "+ fuelLevel);
+        return distance;
+
+    }
+
 
 }
